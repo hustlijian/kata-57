0ac349f994bcffb6d25be3489418121c9e515f41
lijian <hustlijian@gmail.com>
2019-03-06 23:35:44 +0800

    update: add prompt

--- 1/golang/main.go
+++ 1/golang/main.go
@@ -9,20 +9,26 @@ import (
 	"strings"
 )
 
+func getFloat64(reader *bufio.Reader, prompt string) (float64, error) {
+	fmt.Print(prompt)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text)
+	number, err := strconv.ParseFloat(text, 64)
+	return number, err
+}
+
 func main() {
 	var billAmount float64 = 0
 	var tip float64 = 0
 	var tipRate float64 = 0
 	var total float64 = 0
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("What is the bill? ")
-	text, _ := reader.ReadString('\n')
-	text = strings.TrimSpace(text)
-	billAmount, _ = strconv.ParseFloat(text, 64)
-	fmt.Print("What is the tip percentage? ")
-	text, _ = reader.ReadString('\n')
-	text = strings.TrimSpace(text)
-	tipRate, _ = strconv.ParseFloat(text, 64)
+	billAmount, err1 := getFloat64(reader, "What is the bill? ")
+	tipRate, err2 := getFloat64(reader, "What is the tip percentage? ")
+	if err1 != nil || err2 != nil {
+		fmt.Printf("please input number\n")
+		return
+	}
 	tip = math.Ceil(billAmount*tipRate) / 100
 	total = billAmount + tip
 	fmt.Printf("The tip is $%.2f\n", tip)
--- 1/readme.md
+++ 1/readme.md
@@ -9,3 +9,38 @@ What is the tip percentage? 15
 The tip is $30.00
 The total is $230.00
 ```
+
+## test cases
+
+1.
+```
+input:
+10
+15 
+
+output:
+1.50
+11.50
+```
+
+2.
+```
+input:
+11.25
+15
+
+output:
+1.69
+12.94
+```
+
+3.
+```
+input:
+abcd
+15
+
+output:
+please input a number
+```
+
